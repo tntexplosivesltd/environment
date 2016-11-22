98e3986c20a3754887415ed0aa27fa49bf33c9a8
Thomas Phillips <tntexplosivesltd@gmail.com>
2016-11-22 23:33:14 +1300

    Added packages for dwm, added OpenSCAD vim syntax files

--- setup_packages.sh
+++ setup_packages.sh
@@ -3,4 +3,4 @@ wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-ke
 sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
 
 sudo apt-get update
-sudo apt-get install -y conky vim ctags google-chrome-stable qt5-default apcalc tmux colormake ccze git gitk
+sudo apt-get install -y conky vim ctags google-chrome-stable qt5-default apcalc tmux colormake ccze git gitk libxft-dev dwm libpango1.0-dev libxinerama-dev
